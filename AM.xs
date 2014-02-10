garfieldnate/Algorithm-AM

commit 1c83533bcfd15162d01aa5049427eaa9578806a0

add carry and carry_replace macros
Add the `carry` and `carry_replace` macros so that it is easier to
understand what all of the bit-twiddling array logic is. Everytime
there is a `count[i+1] = count[i] >> 16; count[i] &= 0xffff`, that
is just a carry operation for a big integer, which is represented via
`unsigned long[8]` in AM.

AM.xs
@@ -4,6 +4,35 @@
 
 #include "ppport.h"
 
+
+/*
+ * This program must deal with integers that are too big to be
+ * represented by 32 bits.
+ *
+ * They are represented by arrays as
+ *
+ * unsigned long a[8]
+ *
+ * where each a[i] < 2*16.  Such an array represents the integer
+ *
+ * a[0] + a[1] * 2^16 + ... + a[7] * 2^(7*16).
+ *
+ * We only use 16 bits of the unsigned long instead of 32, so that
+ * when we add or multiply two large integers, we have room for overflow.
+ * After any addition or multiplication, the result is carried so that
+ * each element of the array is again < 2*16.
+ *
+ * Someday I may rewrite this in assembler.
+ *
+ */
+#define carry(var, ind) \
+  var[ind + 1] += var[ind] >> 16; \
+  var[ind] &= 0xffff
+
+#define carry_replace(var, ind) \
+  var[ind + 1] = var[ind] >> 16; \
+  var[ind] &= 0xffff
+
 typedef unsigned short AM_SHORT;
 typedef unsigned long AM_LONG;
 
@@ -168,27 +197,6 @@ MGVTBL AMguts_vtab = {
   NULL,
   AMguts_mgFree
 };
-
-/*
- * This program must deal with integers that are too big to be
- * represented by 32 bits.
- *
- * They are represented by arrays as
- *
- * unsigned long a[8]
- *
- * where each a[i] < 2*16.  Such an array represents the integer
- *
- * a[0] + a[1] * 2^16 + ... + a[7] * 2^(7*16).
- *
- * We only use 16 bits of the unsigned long instead of 32, so that
- * when we add or multiply two large integers, we have room for overflow.
- * After any addition or multiplication, the result is normalized so that
- * each element of the array is again < 2*16.
- *
- * Someday I may rewrite this in assembler.
- *
- */
 
 /*
  * arrays used in the change-of-base portion of normalize(SV *s)
@@ -754,25 +762,19 @@ _fillandcount(...)
 	      count[0]  = p0->count;
 
 	      count[0] *= p1->count;
-	      count[1] += count[0] >> 16;
-	      count[0] &= mask;
+        carry(count, 0);
 
 	      count[0] *= p2->count;
 	      count[1] *= p2->count;
-	      count[1] += count[0] >> 16;
-	      count[2] += count[1] >> 16;
-	      count[0] &= mask;
-	      count[1] &= mask;
+        carry(count, 0);
+        carry(count, 1);
 
 	      count[0] *= p3->count;
 	      count[1] *= p3->count;
 	      count[2] *= p3->count;
-	      count[1] += count[0] >> 16;
-	      count[2] += count[1] >> 16;
-	      count[3] += count[2] >> 16;
-	      count[0] &= mask;
-	      count[1] &= mask;
-	      count[2] &= mask;
+        carry(count, 0);
+        carry(count, 1);
+        carry(count, 2);
 
 	      for (i = 0; i < length; ++i)
 		pointercount += (AM_LONG)
@@ -791,39 +793,28 @@ _fillandcount(...)
 		count[1] *= pclo;
 		count[2] *= pclo;
 		count[3] *= pclo;
-		count[1] += count[0] >> 16;
-		count[2] += count[1] >> 16;
-		count[3] += count[2] >> 16;
-		count[4] += count[3] >> 16;
-		count[0] &= mask;
-		count[1] &= mask;
-		count[2] &= mask;
-		count[3] &= mask;
+    carry(count, 0);
+    carry(count, 1);
+    carry(count, 2);
+    carry(count, 3);
+
 		count[1] += hiprod[1];
 		count[2] += hiprod[2];
 		count[3] += hiprod[3];
 		count[4] += hiprod[4];
-		count[2] += count[1] >> 16;
-		count[3] += count[2] >> 16;
-		count[4] += count[3] >> 16;
-		count[5] += count[4] >> 16;
-		count[1] &= mask;
-		count[2] &= mask;
-		count[3] &= mask;
-		count[4] &= mask;
+    carry(count, 1);
+    carry(count, 2);
+    carry(count, 3);
+    carry(count, 4);
 	      } else {
 		count[0] *= pointercount;
 		count[1] *= pointercount;
 		count[2] *= pointercount;
 		count[3] *= pointercount;
-		count[1] += count[0] >> 16;
-		count[2] += count[1] >> 16;
-		count[3] += count[2] >> 16;
-		count[4] += count[3] >> 16;
-		count[0] &= mask;
-		count[1] &= mask;
-		count[2] &= mask;
-		count[3] &= mask;
+    carry(count, 0);
+    carry(count, 1);
+    carry(count, 2);
+    carry(count, 3);
 	      }
 	      for (i = 0; i < length; ++i) {
 		int j;
@@ -842,6 +833,7 @@ _fillandcount(...)
 		p = (AM_LONG *) SvPVX(tempsv);
 		for (j = 0; j < 7; ++j) {
 		  *(p + j) += count[j];
+      /* carry */
 		  *(p + j + 1) += *(p + j) >> 16;
 		  *(p + j) &= mask;
 		}
@@ -962,25 +954,19 @@ _fillandcount(...)
 	      count[0]  = p0->count;
 
 	      count[0] *= p1->count;
-	      count[1] += count[0] >> 16;
-	      count[0] &= mask;
+        carry(count, 0);
 
 	      count[0] *= p2->count;
 	      count[1] *= p2->count;
-	      count[1] += count[0] >> 16;
-	      count[2] += count[1] >> 16;
-	      count[0] &= mask;
-	      count[1] &= mask;
+        carry(count, 0);
+        carry(count, 1);
 
 	      count[0] *= p3->count;
 	      count[1] *= p3->count;
 	      count[2] *= p3->count;
-	      count[1] += count[0] >> 16;
-	      count[2] += count[1] >> 16;
-	      count[3] += count[2] >> 16;
-	      count[0] &= mask;
-	      count[1] &= mask;
-	      count[2] &= mask;
+        carry(count, 0);
+        carry(count, 1);
+        carry(count, 2);
 
 	      for (i = 0; i < length; ++i) {
 		int j;
@@ -999,6 +985,7 @@ _fillandcount(...)
 		p = (AM_LONG *) SvPVX(tempsv);
 		for (j = 0; j < 7; ++j) {
 		  *(p + j) += count[j];
+      /* carry */
 		  *(p + j + 1) += *(p + j) >> 16;
 		  *(p + j) &= mask;
 		}
@@ -1056,20 +1043,17 @@ _fillandcount(...)
     gangcount[0] = 0;
     for (i = 0; i < 6; ++i) {
       gangcount[i] += countlo * p[i];
-      gangcount[i + 1] = gangcount[i] >> 16;
-      gangcount[i] &= 0xffff;
+      carry_replace(gangcount, i);
     }
     if (counthi) {
       for (i = 0; i < 6; ++i) {
 	gangcount[i + 1] += counthi * p[i];
-	gangcount[i + 2] += gangcount[i + 1] >> 16;
-	gangcount[i + 1] &= 0xffff;
+  carry(gangcount, i + 1);
       }
     }
     for (i = 0; i < 7; ++i) {
       grandtotal[i] += gangcount[i];
-      grandtotal[i + 1] += grandtotal[i] >> 16;
-      grandtotal[i] &= 0xffff;
+      carry(gangcount, i);
     }
     grandtotal[7] += gangcount[7];
     tempsv = *hv_fetch(gang, HeKEY(he), 4 * sizeof(AM_SHORT), 1);
@@ -1084,6 +1068,7 @@ _fillandcount(...)
       AM_LONG *s = (AM_LONG *) SvPVX(sum[thisoutcome]);
       for (i = 0; i < 7; ++i) {
 	*(s + i) += gangcount[i];
+  /* carry */
 	*(s + i + 1) += *(s + i) >> 16;
 	*(s + i) &= 0xffff;
       }
@@ -1095,6 +1080,7 @@ _fillandcount(...)
 	AM_LONG *s = (AM_LONG *) SvPVX(sum[ocnum]);
 	for (i = 0; i < 7; ++i) {
 	  *(s + i) += p[i];
+    /* carry */
 	  *(s + i + 1) += *(s + i) >> 16;
 	  *(s + i) &= 0xffff;
 	  dataitem = itemcontextchain[datanum];

commit 224dc3fc3004fcd7f71e1ede466b9daf0d88f405

fix incorrect carry call
Fix a bug introduced in 1c83533b where the wrong variable was
carried. Since `grandtotal` is updated, it should be carried, not
`gangcount`.

AM.xs
@@ -1057,7 +1057,7 @@ _fillandcount(...)
     }
     for (i = 0; i < 7; ++i) {
       grandtotal[i] += gangcount[i];
-      carry(gangcount, i);
+      carry(grandtotal, i);
     }
     grandtotal[7] += gangcount[7];
     tempsv = *hv_fetch(gang, HeKEY(he), 4 * sizeof(AM_SHORT), 1);